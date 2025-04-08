781c8e90b69ab45e474cee30bb50bfe8b9923337
LOI <LOI@LAPTOP-666H3EV3>
2025-04-08 21:26:17 +0700

    phep cong

---
+++ baithuchanh/src/baithuchanh/doannhom.java
@@ -0,0 +1,9 @@
+package baithuchanh;
+
+public static void subtract(Scanner scanner) {
+    System.out.print("Enter first number: ");
+    double num1 = scanner.nextDouble();
+    System.out.print("Enter second number: ");
+    double num2 = scanner.nextDouble();
+    System.out.println("Result: " + (num1 - num2));
+}
