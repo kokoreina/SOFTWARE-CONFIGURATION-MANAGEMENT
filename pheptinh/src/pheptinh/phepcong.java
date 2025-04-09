e836bb6f1f63b8a7552fcf17d32245b196a78de3
ADMIN <ADMIN@MSI.lan>
2025-04-09 11:26:42 +0700

    code phép cộng

---
+++ pheptinh/src/pheptinh/phepcong.java
@@ -0,0 +1,18 @@
+package pheptinh;
+
+import java.util.Scanner;
+
+public class phepcong {
+	 public static void add(Scanner scanner) {
+	        System.out.print("Enter first number: ");
+	        double num1 = scanner.nextDouble();
+	        System.out.print("Enter second number: ");
+	        double num2 = scanner.nextDouble();
+	        System.out.println("Result: " + (num1 + num2));
+	    }
+	public static void main(String[] args) {
+		
+
+	}
+
+}
